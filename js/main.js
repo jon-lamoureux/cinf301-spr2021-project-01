f527bc0db86d790f3197224a65e7582d776df1d6
Jonathan_PL <jlamoureux@stetson.edu>
2021-02-19 15:15:33 -0500

    Made the blank space immovable.

--- js/main.js
+++ js/main.js
@@ -1,30 +1,22 @@
 window.onload = function() {
     var initial = 0;
     const table = document.querySelector('table');
-    const rows = document.querySelectorAll('td');
+    const rows = document.querySelectorAll('tr');
     const rowsArray = Array.from(rows);
-    const finArray = [
-      [0, rowsArray[0].innerHTML],
-      [1, rowsArray[1].innerHTML],
-      [2, rowsArray[2].innerHTML],
-      [0, rowsArray[3].innerHTML],
-      [1, rowsArray[4].innerHTML],
-      [2, rowsArray[5].innerHTML],
-      [0, rowsArray[6].innerHTML],
-      [1, rowsArray[7].innerHTML],
-      [2, rowsArray[8].innerHTML]
-    ];
-    console.log(finArray[3]);
     const solved = "Congratulations, you solved the puzzle!";
 
     table.addEventListener('click', (event) => {
-      const rowIndex = finArray.findIndex(row => row.contains(event.target));
-      const columns = Array.from(finArray[rowIndex].querySelectorAll('td'));
+      const rowIndex = rowsArray.findIndex(row => row.contains(event.target));
+      const columns = Array.from(rowsArray[rowIndex].querySelectorAll('td'));
       const columnIndex = columns.findIndex(column => column == event.target);
-      console.log(rowIndex, columnIndex)
-      switch_elems(rowIndex, columnIndex);
+      console.log(rowIndex, columnIndex);
+      if (event.target.innerHTML != "") {
+        console.log(event.target.id);
+        switch_elems(rowIndex, columnIndex);
+      }
       initial = 1;
     })
+
 
     function switch_elems(i, j) {
       const table = document.querySelector('table');
@@ -39,7 +31,6 @@ window.onload = function() {
 
       table.rows[i].cells[j].innerHTML = val2.toString();
       table.rows[i].cells[k].innerHTML = val1.toString();
-
       if (Boolean(initial) == false) {
         alert(solved);
       }
